e9122a74d3533c4dbf284506c2fdd4ab899d1e8a
mavro <maurofimec@gmail.com>
2020-07-29 16:49:41 -0500

    first comppnent crreated

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import confLogo from '../images/barco1.jpg';
+import "./styles/Badge.css"
 
 class Badge extends React.Component {
 
 render(){
-    return <h1>Badge</h1>
+    
+const firstName= "Mauro";
+const lastName= "Rivero";
+
+    return (
+    <div className="Badge">
+        <div className = "Badge__header">
+            <img src={confLogo} alt="Logo"></img>
+        </div>
+        <div className = "Badge__section-name">
+            <img className = "Badge__avatar" src="https://gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+    <h1 >{this.props.firstName}<br / > {this.props.lastName}</h1>
+        </div>
+        <div className = "Badge__section-info">
+            <h3>{this.props.jobTitle}</h3>
+    <div>@{this.props.twitter}</div>
+        </div>
+
+        <div className = "Badge__footer">
+            #Platziconf
+        </div>
+    </div>)
 }
 }
 
---
+++ src/components/pages/BadgeNew.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class BadgeNew extends React.Component {
+    render(){
+        return (
+            
+        )
+    }
+}
